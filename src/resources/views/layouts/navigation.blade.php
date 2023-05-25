947958f722fff45767a13c423885088a50d750b4
有栖 <sekka.alice@gmail.com>
2023-05-25 21:49:46 +0900

    [update] ナビゲーションにグループ一覧とグループ作成のリンクを追加。

--- src/resources/views/layouts/navigation.blade.php
+++ src/resources/views/layouts/navigation.blade.php
@@ -12,8 +12,14 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('group.index')" :active="request()->routeIs('group.index')">
+                        {{ __('Groups') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('group.create')" :active="request()->routeIs('group.create')">
+                        {{ __('Create Group') }}
                     </x-nav-link>
                 </div>
             </div>
